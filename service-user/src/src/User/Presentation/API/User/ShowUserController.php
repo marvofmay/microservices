ed8917cb26916cab87b0a1606f5c48a1d5e9a2f7
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-03-23 22:50:02 +0100

    GetUser - PHP8 refactor

--- service-user/src/src/User/Presentation/API/User/ShowUserController.php
+++ service-user/src/src/User/Presentation/API/User/ShowUserController.php
@@ -8,30 +8,17 @@ use App\User\Domain\Service\User\ReaderService\UserReaderService;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/users', name: 'api.users.')]
 class ShowUserController extends AbstractController
 {
-    private UserReaderService $userReaderService;
-    public LoggerInterface $logger;
-    public UserPasswordHasherInterface $userPasswordInterface;
-    public SerializerInterface $serializer;
-
     public function __construct(
-        UserReaderService   $userReaderService,
-        LoggerInterface             $logger,
-        UserPasswordHasherInterface $userPasswordInterface,
-        SerializerInterface         $serializer
-    )
-    {
-        $this->userReaderService = $userReaderService;
-        $this->logger = $logger;
-        $this->userPasswordInterface = $userPasswordInterface;
-        $this->serializer = $serializer;
-    }
+        private readonly UserReaderService $userReaderService,
+        private readonly LoggerInterface $logger,
+        private readonly SerializerInterface $serializer
+    ) {}
 
     #[Route('/{uuid}', name: 'show', methods: ['GET'])]
     public function show(string $uuid): Response
@@ -50,7 +37,7 @@ class ShowUserController extends AbstractController
         } catch (\Exception $e) {
             $this->logger->error('show user by uuid: ' . $e->getMessage());
 
-            return $this->json(['errors' => $e->getMessage()],Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->json(['errors' => 'Upss... problem with show user data'],Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
